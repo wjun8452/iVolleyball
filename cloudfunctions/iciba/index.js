8d02467a5570650f3ab3c23f2a1a5d4c361b313a
junwang <wjun_8452@163.com>
2020-10-07 18:42:04 +0800

    v3.8.6, 首页增加词霸金句，优化首页

---
+++ cloudfunctions/iciba/index.js
@@ -0,0 +1,18 @@
+// 云函数模板
+// 部署：在 cloud-functions/iciba 文件夹右击选择 “上传并部署”
+
+const cloud = require('wx-server-sdk')
+var rp = require('request-promise');
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  let url = 'http://open.iciba.com/dsapi/';
+  return await rp(url)
+    .then(function (res) {
+      return res
+    })
+    .catch(function (err) {
+      return '失败'
+    });
+}
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,32 +3,66 @@
 var app = getApp()
 Page({
   data: {
-    userInfo: {}
+    userInfo: {},
+    iciba: "", //每日一句英语
+    marqueeDistance: -30, //初始滚动距离
+    windowHeight: 0,
+    windowWidth: 0,
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
 
-  onShareAppMessage: function(e) {
-  },
+  onShareAppMessage: function (e) {},
 
   onLoad: function (options) {
+    //
+    var res = wx.getSystemInfoSync()
+    //can not use this.data.height to set score_height
+    this.data.windowHeight = res.windowHeight
+    this.data.windowWidth = res.windowWidth
+    this.setData(this.data)
+
     var that = this
+
+    //获取每日一句正能量名言
+    wx.cloud.callFunction({
+      name: 'iciba',
+      data: {},
+      success: res => {
+        var obj = JSON.parse(res.result)
+        that.data.iciba = obj.content
+        console.log('[iciba]', that.data.iciba)
+        that.setData(that.data)
+
+        //开始滚动显示金句
+        var length = that.data.iciba.length * 14; //文字长度
+        var windowWidth = wx.getSystemInfoSync().windowWidth; // 屏幕宽度
+        that.setData({
+          length: length,
+          windowWidth: windowWidth
+        });
+        that.scrolltxt(); // 第一个字消失后立即从右边出现
+      },
+      fail: err => {
+        console.error('[iciba] failed!', err)
+      }
+    })
+
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
 
     if (options._id != null && options._id != undefined) {
       this.data._id = options._id
-    } else {
-    }
+    } else {}
 
     if (options._openid != null && options._openid != undefined) {
       this.data._openid = options._openid
@@ -38,5 +72,43 @@ Page({
     }
 
     this.setData(this.data)
-  }
+  },
+
+  onShow: function () {
+  },
+
+  scrolltxt: function () {
+    var that = this;
+    var length = that.data.iciba.length * 7; //滚动文字的宽度
+    var windowWidth = that.data.windowWidth; //屏幕宽度
+    console.log(windowWidth)
+    if (length > windowWidth) {
+      var interval = setInterval(function () {
+        var maxscrollwidth = length; //滚动的最大宽度，文字宽度+间距，如果需要一行文字滚完后再显示第二行可以修改marquee_margin值等于windowWidth即可
+        var crentleft = that.data.marqueeDistance;
+        //console.log(crentleft, maxscrollwidth)
+        if (crentleft < maxscrollwidth) { //判断是否滚动到最大宽度
+          that.setData({
+            marqueeDistance: crentleft + 1
+          })
+        } else {
+          //console.log("替换");
+          that.setData({
+            marqueeDistance: 0 // 直接重新滚动
+          });
+          clearInterval(interval);
+          that.scrolltxt();
+        }
+      }, 60);
+    } else {
+      that.setData({
+        marquee_margin: "1000"
+      }); //只显示一条不滚动右边间距加大，防止重复显示
+    }
+  },
+
+
+  stopPageScroll: function () {
+    return
+  },
 })

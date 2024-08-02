13cd7028bf975b8286a5c30b66e05fbd29231a63
Wang Jun <wjun_8452@163.com>
2024-08-02 12:32:53 +0800

    增加几个新闻源,并不再删除DB

--- cloudfunctions/news/index.js
+++ cloudfunctions/news/index.js
@@ -13,20 +13,34 @@ const db = cloud.database({ env: cloud.DYNAMIC_CURRENT_ENV })
 
 // {"creator":"","title":"“FIVB世界女排联赛香港2023”将于6月中旬在..","link":"https://baijiahao.baidu.com/s?id=1763876970868891712","pubDate":"2023-04-22T12:19:58.000Z","author":"","content":"<a target=\"_blank\" href=http://baijiahao.baidu.com/s?id=1763876970868891712><img border=\"0\" src=\"http://t1.baidu.com/it/u=http%3A%2F%2Ft11.baidu.com%2Fit%2Fu%3D3026368709%2C202316589%26fm%3D30%26app%3D106%26f%3DJPEG%3Fw%3D312%26h%3D208%26s%3D3F23488442B281D2461A1F910300D08E&fm=30\"></a><br>主礼嘉宾为赛事进行启动仪式。　陈永诺　摄中新网香港4月22日电(记者魏华都)香港排球总会22日召开记者会宣布，“中国人寿(海外)FIVB世界女排联赛香港2023”将于6月13日至18日在香港体育馆举行","contentSnippet":"主礼嘉宾为赛事进行启动仪式。　陈永诺　摄中新网香港4月22日电(记者魏华都)香港排球总会22日召开记者会宣布，“中国人寿(海外)FIVB世界女排联赛香港2023”将于6月13日至18日在香港体育馆举行","isoDate":"2023-04-22T12:19:58.000Z"}
 addNewsToDB = async function (item) {
-  const result = await db.collection("vnews").add({
-    data: {
-      creator: item.creator,
-      title: item.title,
-      link: item.link, //detail news link
-      pubDate: item.pubDate,
-      author: item.author,
-      content: item.content,
-      contentSnippet: item.contentSnippet,
-      isoDate: item.isoDate,
-      updateTime: new Date(),
-    }
-  })
+  const _ = db.command
+  const result = await db.collection('vnews').where({
+    title: item.title
+  }).get()
   console.log(result)
+  if (result.data && result.data.length > 0) {
+    console.log("duplicated record, abandon adding.")
+    return;
+  }
+
+  try {
+    const result = await db.collection("vnews").add({
+      data: {
+        creator: item.creator,
+        title: item.title,
+        link: item.link, //detail news link
+        pubDate: item.pubDate,
+        author: item.author,
+        content: item.content,
+        contentSnippet: item.contentSnippet,
+        isoDate: item.isoDate,
+        updateTime: new Date(),
+      }
+    })
+    console.log(result)
+  } catch(e) {
+    console.error(e)
+  }
 }
 
 clearDB = async function () {
@@ -77,18 +91,21 @@ loadNewsDetail = async function (url) {
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  let urls = ['https://feedx.net/rss/thepaper.xml',
-    'https://feedx.net/rss/infzm.xml'
-    //'https://news.baidu.com/n?cmd=4&class=volleyball&tn=rss' //百度停止更新了
+  //越靠后，新闻越靠前显示
+  let urls = [
+    'https://news.baidu.com/n?cmd=4&class=volleyball&tn=rss', //百度停止更新了
+    'https://news.baidu.com/n?cmd=1&class=sportnews&tn=rss',
+    'https://feedx.net/rss/bjnews.xml',
+    'https://feedx.net/rss/infzm.xml',
+    'https://feedx.net/rss/thepaper.xml'
   ];
-  let keywords = '排球|男排|女排|沙排|气排|奥运|世锦赛'
+  let keywords = '排球|男排|女排|沙排|气排'
   let news = new Array();
 
   for (const index in urls) {
     console.log("collect RSS news starts, from ", urls[index])
     let feed = await parser.parseURL(urls[index]);
     console.log("total news: ", feed.items.length, ", from ", feed.title);
-    console.log("collect RSS news ends.")
 
     for (const index2 in feed.items) {
       console.log(index2, ": ", feed.items[index2].title);
@@ -99,13 +116,14 @@ exports.main = async (event, context) => {
     }
   }
 
-  await clearDB()
+  //todo: 后续再删除发布日期在3个月之前的新闻
+  //await clearDB()
 
-  console.log("***** 排球新闻总条数:  ", news.length)
+  console.log("***** 排球新闻总条数:  ", news.length, ". 保存到数据库 ...")
   for (const index in news) {
+    console.log(index, ": ", news[index].title);
     await addNewsToDB(news[index])
   }
-  console.log("saved to db.")
 
   //访问摘要的link，爬取新闻的细节
   // for (const index in [0]) {
